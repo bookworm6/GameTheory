d43094709a09e216f9cecf2c7c1a654f52ddfb6d
bookworm6 <bilborulesmordor@gmail.com>
2025-11-16 15:52:40 -0800

    made a version that doesn't multi thread for performance comparison

--- experiments.py
+++ experiments.py
@@ -276,7 +276,7 @@ def superPlot(tracker="experiments.txt"):
 
 
 builder = ExperimentBuilder("./sim")
-paramdict = {"repeats": 1, "rounds": 100000, "snaps": 10, "gridN": 128, "varySeed": False, 
+paramdict = {"repeats": 1, "rounds": 100, "snaps": 10, "gridN": 32, "varySeed": False, 
                             "payoffMatrix": [[1,5],[0,3.3]], "inversionPercentage": 0.1,
                             "mutationRate": 0.005, "res": (2,2)} #goal gridN : 128
 exp = builder.fromParamDict(paramdict)
